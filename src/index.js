1ccacbece452d99439dd7402be8b374f8a483f55
Otávio Barreto de Oliveira Silva <thor@pop-os.localdomain>
2019-10-25 18:30:57 -0300

    Removido props de conteudo e action e passado pro children

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import { render } from "react-dom"
-
-import { createGlobalStyle } from 'styled-components'
+  
 import Card from './lib/index'
 import logo from './lib/assets/icon.png';
 
-const ResetCSS = createGlobalStyle `
-  body {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-`
 
 class App extends React.Component {
 
@@ -29,76 +21,14 @@ class App extends React.Component {
   render() {
     return (
       <>
-        {/* <ResetCSS/>
-          <Navbar img={logo} items={[
-          {
-            icon: 'I',
-            name: 'Deu certo'
-          },{
-            icon: 'I',
-            name: 'Deu certo'
-          }
-        ]}> */}
-        <div>
-          <Card
-            title="Card Teste"
-            titleComplement="Teste teste"
-            header={logo}
-            subtitle="subtitle do card"
-            actionAlign="left"
-            actions={[{
-              name: "Click me",
-              appearance: "contained",
-              handler: (event) => {
-                console.log(event)
-              }
-            }, {
-              name: "Oi",
-              appearance: "text",
-              handler: alert
-            }]}
-          />
-          <Card
-            title="Card Teste 2"
-            titleComplement="Teste teste 3"
-            header={logo}
-            subtitle="subtitle do card"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            actionAlign="center"
-            actions={[{
-              name: "Click me",
-              appearance: "contained",
-              handler: (event) => {
-                console.log(event)
-              }
-            }, {
-              name: "Oi",
-              appearance: "outlined",
-              handler: alert
-            }]}
-          />
-          <Card
-            title="Card Teste 2"
-            titleComplement="Teste teste 3"
-            header={logo}
-            subtitle="subtitle do card"
-            description="Lorem ipsum dolor sit amet."
-            actionAlign="right"
-            actions={[{
-              name: "Click me",
-              appearance: "contained",
-              handler: (event) => {
-                console.log(event)
-              }
-            }, {
-              name: "Oi",
-              appearance: "outlined",
-              handler: alert
-            }]}
-          />
-        </div>
-
-        {/* </Navbar> */}
+        <Card
+          title="Card Teste"
+          titleComplement="Teste teste"
+          header={logo}
+          subtitle="subtitle do card"
+        >
+          Thor ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Card>
       </>
     )
   }
--- src/lib/components/CardComponent.js
+++ src/lib/components/CardComponent.js
@@ -1,38 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-import {ResetCSS, Card, CardHeader, CardTitle, CardTitleComplement, CardSubtitle, CardContent, CardDescription, CardContainerAction, CardAction} from './Style';
+import { ResetCSS, Card, CardHeader, CardTitle, CardTitleComplement, CardSubtitle, CardContent } from './Style';
 
-  const CardComponent = ({header, title, titleComplement, subtitle, description, actionAlign, actions}) => {
-    return (
-      <Card className="card-component-module">
-        <ResetCSS/>
-        {header && <CardHeader header={header}/>}
-        <CardTitle title={title}>{title}</CardTitle>
-        <CardTitleComplement>{titleComplement}</CardTitleComplement>
-        <CardSubtitle>{subtitle}</CardSubtitle>
-        {description &&
-          <CardContent>
-            <CardDescription>{description}</CardDescription>
-          </CardContent>
-        }
-        <CardContainerAction actionAlign={actionAlign}>
-          {actions && actions.map((action, i) => {
-            return (
-              <CardAction key={i} appearance={action.appearance} onClick={action.handler}>{action.name}</CardAction>
-            )
-          })}
-        </CardContainerAction>
-      </Card>
-      )
-  }
+const CardComponent = ({ header, title, titleComplement, subtitle, children }) => {
+  return (
+    <Card className="card-component-module">
+      <ResetCSS />
+      {header && <CardHeader header={header} />}
+      <CardTitle title={title}>{title}</CardTitle>
+      <CardTitleComplement>{titleComplement}</CardTitleComplement>
+      <CardSubtitle>{subtitle}</CardSubtitle>
+      <CardContent>
+        {children}
+      </CardContent>
+    </Card>
+  )
+}
 
-  CardComponent.propTypes = {
-    header: PropTypes.string,
-    title: PropTypes.string.isRequired,
-    titleComplement: PropTypes.node,
-    subtitle: PropTypes.string,
-    description: PropTypes.string
-  }
+CardComponent.propTypes = {
+  header: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  titleComplement: PropTypes.node,
+  subtitle: PropTypes.string,
+  description: PropTypes.string
+}
 
-  export default CardComponent
+export default CardComponent
